1b510abb5952937661855c2a9bf0c97c942cf7cf
tmcknew <tmcknew@users.noreply.github.com>
2018-09-03 22:05:23 -0700

    adds activity labels, rbinds the two datasets

--- run_analysis.R
+++ run_analysis.R
@@ -10,13 +10,18 @@
 #   5. From the data set in step 4, creates a second, independent 
 #   tidy data set with the average of each variable for each activity 
 #   and each subject.
+
 rm(list = ls())
 # we will use "fread" from "data.table" to read the data
 install.packages("data.table")
-library(data.table)
 # and dplyr for tidying
 install.packages("dplyr")
+# and tibble for tibbles
+install.packages("tibble")
+
+library(data.table)
 library(dplyr)
+library(tibble)
 
 # ensure files are present
 if (!file.exists("UCI HAR Dataset/")) {
@@ -25,29 +30,74 @@ if (!file.exists("UCI HAR Dataset/")) {
     unzip(zipfile = "UCI HAR Dataset.zip" )
 }
 
-# load observations ("X_") from train/ and test/
-X_train <- fread("UCI HAR Dataset/train/X_train.txt")
-X_test <- fread("UCI HAR Dataset/test/X_test.txt")
-# convert to tibble 
-X_train <- as_tibble(X_train)
-X_test <- as_tibble(X_test)
+# load observations ("X_") from test/ and train/
+X_test <- fread("UCI HAR Dataset/test/X_test.txt") %>% as_tibble
+X_train <- fread("UCI HAR Dataset/train/X_train.txt") %>% as_tibble
 
-# read in the labels (column names) from features.txt
+# read labels (column names) from features.txt
 X_labels <- read.csv("UCI HAR Dataset/features.txt", 
                      sep = " ", header = FALSE,
                      col.names = c("index", "name"),
-                     colClasses = c("integer", "character") )
-X_labels$name <- gsub('(\\(|\\)|,)',"",X_labels$name)
-X_labels
+                     colClasses = c("integer", "character") ) %>%
+                        as_tibble
+                        
 # assign labels to columns of the data frames
-colnames(X_train) <- X_labels$name
 colnames(X_test) <- X_labels$name
-# rm(X_labels)
+colnames(X_train) <- X_labels$name
+
+# import activity data for the test data set as a factor
+# then cbind to X_test
+read.csv("UCI HAR Dataset/test/y_test.txt", header = FALSE, 
+         col.names = c("activity"), colClasses = c("factor") ) %>%
+    as_tibble %>%
+    cbind(X_test) %>%
+    set_tidy_names %>%
+    assign(x = "X_test", ., pos=1)
+
+# import activity data for the training data set as a factor
+# then cbind to X_train
+read.csv("UCI HAR Dataset/train/y_train.txt", header = FALSE, 
+         col.names = c("activity"), colClasses = c("factor") ) %>%
+    as_tibble %>%
+    cbind(X_train) %>%
+    set_tidy_names %>%
+    assign(x = "X_train", ., pos=1)
+
+data.frame(datasource = rep("X_test.txt", nrow(X_test))) %>%
+    cbind(X_test) %>%
+    assign(x = "X_test", ., pos=1)
+
+data.frame(datasource = rep("X_train.txt", nrow(X_train))) %>%
+    cbind(X_train) %>%
+    assign(x = "X_train", ., pos=1)
+
+obs <- rbind(X_train,X_test) %>% as_tibble
+
+# read activity labels
+activitylabels <- 
+    read.csv("UCI HAR Dataset/activity_labels.txt", sep = " ", 
+                header = FALSE, col.names = c("index", "activity"),
+                colClasses = c("integer", "character") ) %>%
+                    mutate(activity = tolower(activity))
+
+levels(obs$activity)[levels(obs$activity)==activitylabels[,"index"]] <- 
+    activitylabels[,"activity"]
+
+    
+#   ^^^^^ OK, needs work vvvvvv
+obs %>% 
+    mutate(activity_label = activity_labels[activity,"activity"]) %>%
+    assign(x="obs", ., pos = 1)
+
+activity_train %>%
+    as_tibble %>%
+    mutate(activity_label = activity_labels[activity,"activity"]) %>%
+    assign(x="activity_test", ., pos = 1)
 
 # add logical columns to indicate source before rbinding train and test
-X_train_src = data.frame(from_X_train = rep(TRUE, nrow(X_train)),
+X_train_src <- data.frame(from_X_train = rep(TRUE, nrow(X_train)),
                          from_X_test = rep(FALSE, nrow(X_train)))
-X_test_src = data.frame(from_X_train = rep(FALSE, nrow(X_test)),
+X_test_src <- data.frame(from_X_train = rep(FALSE, nrow(X_test)),
                         from_X_test = rep(TRUE, nrow(X_test)))
 # bind columns
 X_train <- cbind(X_train_src,X_train)
@@ -55,32 +105,21 @@ X_test <- cbind(X_test_src,X_test)
 # rm(X_train_src); rm(X_test_src) 
 
 # bind rows from train and test
-obs <- rbind(X_train, X_test)
+obs <- set_tidy_names(rbind(X_train, X_test))
 
-select(obs, -contains("bandsEnergy"))
+mean_cols <- grepl("mean", colnames(obs))
+std_cols <- grepl("std", colnames(obs))
+source_cols <- grepl("from_", colnames(obs))
 
-X_train %>%
-    select( contains("mean"), contains("std") )
+select(obs, which(mean_cols | std_cols) ) %>%
+    as_tibble
+
+obs %>%
+    select( contains("mean"), contains("std") ) %>%
     as_tibble %>%
     group_by(from_X_train) %>%
     summarise(n = n())
-# import activity data for the training data set as a factor
-activity_train <-  read.csv("UCI HAR Dataset/train/y_train.txt",
-                            header = FALSE, col.names = c("activity"),
-                            colClasses = c("factor") )
 
-# import activity data for the test data set as a factor
-activity_test <-  read.csv("UCI HAR Dataset/test/y_test.txt", 
-                            header = FALSE, col.names = c("activity"),
-                            colClasses = c("factor") )
-
-# read activity labels
-activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", 
-                     sep = " ", header = FALSE,
-                     col.names = c("index", "activity"),
-                     colClasses = c("integer", "character") )
-short <- head(activity_test)
-activity_test %>%
-    as_tibble %>%
-    mutate(activity_label = activity_labels[activity,"activity"]) %>%
-    assign(x="activity_test", ., pos = 1)
+for (v in levels(a)) {
+    print(paste0(v,"/X_",v,".txt") )
+}
