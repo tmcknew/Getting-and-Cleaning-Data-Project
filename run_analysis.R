fd4bb931e5d285dc92d44abd1899ad1baf1f001d
tmcknew <tmcknew@users.noreply.github.com>
2018-09-13 03:06:17 -0700

    adds comments for objectives

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 # run_analysis.R
-# 
+#
 #   Objectives:
 #   1. Merges the training and the test sets to create one data set.
 #   2. Extracts only the measurements on the mean and standard deviation 
@@ -66,7 +66,7 @@ read.csv("UCI HAR Dataset/train/y_train.txt", header = FALSE,
     set_tidy_names(quiet = TRUE) %>%
     assign(x = "X_train", ., pos = 1)
 
-# import subject data for the test data set as a an integer
+# import subject data for the test data set as an integer
 read.csv("UCI HAR Dataset/test/subject_test.txt", header = FALSE, 
          col.names = "subject", colClasses = "integer" ) %>%
     as_tibble %>%
@@ -80,18 +80,11 @@ read.csv("UCI HAR Dataset/train/subject_train.txt", header = FALSE,
     cbind(X_train) %>%
     assign(x = "X_train", ., pos = 1)
 
-# # add source column to dataset before merging with rbind
-# data.frame(datasource = rep("test", nrow(X_test))) %>%
-#     cbind(X_test) %>%
-#     assign(x = "X_test", ., pos = 1)
-# 
-# # add source column to dataset before merging with rbind
-# data.frame(datasource = rep("train", nrow(X_train))) %>%
-#     cbind(X_train) %>%
-#     assign(x = "X_train", ., pos = 1)
-
 # combine train and test datasets
-obs <- rbind(X_train,X_test) %>% as_tibble
+X_all <- rbind(X_train,X_test) %>% as_tibble
+
+#   Objective 1 complete:
+#   1. Merges the training and the test sets to create one data set.
 
 # read activity labels
 activitylabels <- 
@@ -100,35 +93,61 @@ activitylabels <-
                 colClasses = c("integer", "character") ) %>%
                     mutate(activity = tolower(activity))
 
-# set factor levels in obs to activitylabels[,"activity"]
-levels(obs$activity)[levels(obs$activity) == activitylabels[,"index"]] <- 
+# set factor levels in X_all to activitylabels[,"activity"]
+levels(X_all$activity)[levels(X_all$activity) == activitylabels[,"index"]] <- 
     activitylabels[,"activity"]
 
+#   Objective 3 Complete:
+#   3. Uses descriptive activity names to name the activities in the 
+#   data set
+
+# some logical vectors for columns we want to keep 
+# ... (subject, activity, means and stdevs.)
+first_cols <- c(rep(TRUE,2),rep(FALSE,length(colnames(X_all)) - 2))
+mean_cols <- grepl("mean", colnames(X_all))
+std_cols <- grepl("std", colnames(X_all))
+
+# select on columns to keep
+mean_std_X_all <- X_all %>%
+    select(which(first_cols | mean_cols | std_cols) )
+
+#   Objective 2 Complete:
+#   2. Extracts only the measurements on the mean and standard deviation 
+#   for each measurement.
+
 # fix column names to be friendlier
-testnames <- sample(1:length(names(obs)),6)
-names(obs) %>%
+names(mean_std_X_all) <- names(mean_std_X_all) %>%
     gsub("^t","timeseries_",.) %>%
     gsub("^f","fft_",.) %>%
     gsub("Body","body_",.) %>%
     gsub("Gravity","gravity_",.) %>%
-    gsub("Acc","acceleration_vector_",.) %>%
+    gsub("Acc","acceleration_",.) %>%
     gsub("Gyro","angular_velocity_",.) %>%
+    gsub("Jerk","jerk_",.) %>%
     gsub("Mag","magnitude_",.) %>%
-    gsub("(bandsEnergy\\(\\)-)([0-9]+),([0-9]+)(..[0-9]+)",
-         "energyband_\\2_\\3",.) %>%
-        .[testnames]
-names(obs)[testnames]
+    gsub("-X","x_component",.) %>%
+    gsub("-Y","y_component",.) %>%
+    gsub("-Z","z_component",.) %>%
+    gsub("std","standard_deviation",.) %>%
+    gsub("meanFreq","frequency_mean",.) %>%
+    gsub("_body_body","_body",.) %>%
+    
+    gsub("(-)(\\w*)(\\(\\))(\\w*)",
+        "\\4_\\2",.) %>%
+    gsub("__","_",.)
+    
+#   Objective 4 Complete:
+#   4. Appropriately labels the data set with descriptive variable names.
+#   names(mean_std_X_all)
 
-# collect columns we want to keep (first 2, means and stdevs.)
-first_cols <- c(rep(TRUE,2),rep(FALSE,length(colnames(obs)) - 2))
-mean_cols <- grepl("mean", colnames(obs))
-std_cols <- grepl("std", colnames(obs))
-
-# group by subject, activity and summarise
-avg_obs <- obs %>%
-    select(which(first_cols | mean_cols | std_cols) ) %>%
+summary_X_all <- mean_std_X_all %>%
     arrange(activity, subject) %>%
     group_by(activity, subject) %>%
-    summarise_if(is.numeric, mean) %>%
-    select(activity, subject) %>%
-    print
+    summarise_if(is.numeric, mean)
+
+#   Objective 5 Complete:
+#   5. From the data set in step 4, creates a second, independent 
+#   tidy data set with the average of each variable for each activity 
+#   and each subject.
+
+write.table(summary_X_all, file = "out.txt", row.name = FALSE)
